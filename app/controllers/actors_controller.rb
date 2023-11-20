6c72b45c1629c58c3a2f1b96b45750410a2d670c
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-20 19:52:33 +0000

    Actors Test

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,48 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+
+
+  def create
+
+    the_actor = Actor.new
+    the_actor.name = params.fetch("query_name")
+    the_actor.year = params.fetch("query_dob") 
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+
+ 
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+     matching_records = Actor.where({ :id => the_id })
+     the_actor = matching_records.at(0)
+     the_actor.destroy
+     redirect_to("/actors")
+  end
+
+  def update
+    
+    the_id = params.fetch("the_id")
+    matching_records = Actor.where({ :id => the_id})
+    the_actor = matching_records.at(0)
+
+    the_actor = Actor.new
+    the_actor.name = params.fetch("query_name")
+    the_actor.year = params.fetch("query_dob") 
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,18 +17,18 @@ class MoviesController < ApplicationController
 
   def create
 
-    n = Movie.new
-    n.title = params.fetch("query_title")
-    n.year = params.fetch("query_year")
-    n.duration = params.fetch("query_duration")
-    n.description = params.fetch("query_description")
-    n.image = params.fetch("query_image")
-    n.director_id = params.fetch("query_director_id")
+    the_movie = Movie.new
+    the_movie.year = params.fetch("query_year")
+    the_movie.title = params.fetch("query_title")
+    the_movie.duration = params.fetch("query_duration")
+    the_movie.description = params.fetch("query_description")
+    the_movie.image = params.fetch("query_image")
+    the_movie.director_id = params.fetch("query_director_id")
 
 
-    n.save
+    the_movie.save
 
-    redirect_to("/movies")
+    redirect_to("/movies/#{the_movie.id}")
 
  
   end
@@ -39,8 +39,27 @@ class MoviesController < ApplicationController
      the_movie = matching_records.at(0)
      the_movie.destroy
      redirect_to("/movies")
+  end
 
+  def update
+    
+    n_id = params.fetch("the_id")
+    matching_records = Movie.where({ :id => n_id})
+    the_movie = matching_records.at(0)
+
+    the_movie.title = params.fetch("query_title")
+    the_movie.year = params.fetch("query_year")
+    the_movie.duration = params.fetch("query_duration")
+    the_movie.description = params.fetch("query_description")
+    the_movie.image = params.fetch("query_image")
+    the_movie.director_id = params.fetch("query_director_id")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
 
   end
+
+  
 
 end
